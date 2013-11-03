6e34a51487f944f35d76495d34a9b2b840ffb7b5
sneed <midtest17@gmail.com>
2013-11-03 20:58:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var buffer = new Buffer("Hello World from index.html!", "utf-8");
+var fs = require("fs");
+var fileName = "index.html";
+var contents = fs.readFileSync("./index.html", "utf8").toString();
+
+console.log(contents);
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('./index.html', function(request, response) {
+  response.send('Hello World from index.html');
 });
 
 var port = process.env.PORT || 5000;
